6814ea2a0089ef96fd9cb7888887f775199c8c6a
lepipele <lepipele@users.noreply.github.com>
2015-02-23 04:33:42 -0600

    Original article source code, Visual Studio 2008, pre-Windows 7, .NET Framework 3.5

---
+++ Demo/MainForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Demo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.comboBoxCameras = new System.Windows.Forms.ComboBox();
+            this.btnConfig = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(516, 515);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(87, 23);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save current";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(684, 497);
+            this.pictureBoxDisplay.TabIndex = 13;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // comboBoxCameras
+            // 
+            this.comboBoxCameras.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.comboBoxCameras.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCameras.FormattingEnabled = true;
+            this.comboBoxCameras.Location = new System.Drawing.Point(185, 516);
+            this.comboBoxCameras.Name = "comboBoxCameras";
+            this.comboBoxCameras.Size = new System.Drawing.Size(153, 21);
+            this.comboBoxCameras.TabIndex = 12;
+            // 
+            // btnConfig
+            // 
+            this.btnConfig.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConfig.Location = new System.Drawing.Point(609, 515);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(87, 23);
+            this.btnConfig.TabIndex = 10;
+            this.btnConfig.Text = "Configuration";
+            this.btnConfig.UseVisualStyleBackColor = true;
+            this.btnConfig.Click += new System.EventHandler(this.btnConfig_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStop.Location = new System.Drawing.Point(93, 515);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStart.Location = new System.Drawing.Point(12, 515);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 550);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Controls.Add(this.comboBoxCameras);
+            this.Controls.Add(this.btnConfig);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.MinimumSize = new System.Drawing.Size(640, 520);
+            this.Name = "MainForm";
+            this.Text = "WebCam Demo";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.ComboBox comboBoxCameras;
+        private System.Windows.Forms.Button btnConfig;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Demo/MainForm.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Touchless.Vision.Camera;
+
+namespace Demo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (!DesignMode)
+            {
+                // Refresh the list of available cameras
+                comboBoxCameras.Items.Clear();
+                foreach (Camera cam in CameraService.AvailableCameras)
+                    comboBoxCameras.Items.Add(cam);
+
+                if (comboBoxCameras.Items.Count > 0)
+                    comboBoxCameras.SelectedIndex = 0;
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            thrashOldCamera();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            thrashOldCamera();
+        }
+
+        private CameraFrameSource _frameSource;
+        private static Bitmap _latestFrame;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Early return if we've selected the current camera
+            if (_frameSource != null && _frameSource.Camera == comboBoxCameras.SelectedItem)
+                return;
+
+            thrashOldCamera();
+            startCapturing();
+        }
+
+        private void startCapturing()
+        {
+            try
+            {
+                Camera c = (Camera)comboBoxCameras.SelectedItem;
+                setFrameSource(new CameraFrameSource(c));
+                _frameSource.Camera.CaptureWidth = 320;
+                _frameSource.Camera.CaptureHeight = 240;
+                _frameSource.Camera.Fps = 20;
+                _frameSource.NewFrame += OnImageCaptured;
+
+                pictureBoxDisplay.Paint += new PaintEventHandler(drawLatestImage);
+                _frameSource.StartFrameCapture();
+            }
+            catch (Exception ex)
+            {
+                comboBoxCameras.Text = "Select A Camera";
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void drawLatestImage(object sender, PaintEventArgs e)
+        {
+            if (_latestFrame != null)
+            {
+                // Draw the latest image from the active camera
+                e.Graphics.DrawImage(_latestFrame, 0, 0, _latestFrame.Width, _latestFrame.Height);
+            }
+        }
+
+        public void OnImageCaptured(Touchless.Vision.Contracts.IFrameSource frameSource, Touchless.Vision.Contracts.Frame frame, double fps)
+        {
+            _latestFrame = frame.Image;
+            pictureBoxDisplay.Invalidate();
+        }
+
+        private void setFrameSource(CameraFrameSource cameraFrameSource)
+        {
+            if (_frameSource == cameraFrameSource)
+                return;
+
+            _frameSource = cameraFrameSource;
+        }
+
+        //
+
+        private void thrashOldCamera()
+        {
+            // Trash the old camera
+            if (_frameSource != null)
+            {
+                _frameSource.NewFrame -= OnImageCaptured;
+                _frameSource.Camera.Dispose();
+                setFrameSource(null);
+                pictureBoxDisplay.Paint -= new PaintEventHandler(drawLatestImage);
+            }
+        }
+
+        //
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (_frameSource == null)
+                return;
+
+            Bitmap current = (Bitmap)_latestFrame.Clone();
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "*.bmp|*.bmp";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    current.Save(sfd.FileName);
+                }
+            }
+
+            current.Dispose();
+        }
+
+        private void btnConfig_Click(object sender, EventArgs e)
+        {
+            // snap camera
+            if (_frameSource != null)
+                _frameSource.Camera.ShowPropertiesDialog();
+        }
+    }
+}
